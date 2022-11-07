691781a87cc7a5dfe13b9a68a6bfbeae9b05ca78
nosbeto <114036566+nosbeto@users.noreply.github.com>
2022-11-07 10:26:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return timeIsOver()
+        return
     }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
